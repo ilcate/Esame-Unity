7aedfc19b3b2dcffa4ee6141e29580104b3c7d21
ilcate <124363106+ilcate@users.noreply.github.com>
2024-05-01 10:50:51 +0200

    testlogejoin

---
+++ Assets/Scripts/Logger.cs
@@ -0,0 +1,63 @@
+using System.Linq;
+using DilmerGames.Core.Singletons;
+using TMPro;
+using UnityEngine;
+using System;
+
+public class Logger : Singleton<Logger>
+{
+    [SerializeField] private TextMeshProUGUI debugAreaText = null;
+    [SerializeField] private bool enableDebug = false;
+    [SerializeField] private int maxLines = 15;
+
+    void Awake()
+    {
+        if (debugAreaText == null)
+        {
+            debugAreaText = GetComponent<TextMeshProUGUI>();
+        }
+        debugAreaText.text = string.Empty;
+    }
+
+    void OnEnable()
+    {
+        debugAreaText.enabled = enableDebug;
+        enabled = enableDebug;
+
+        if (enabled)
+        {
+            LogMessage($"{this.GetType().Name} enabled", Color.white);
+        }
+    }
+
+    private void LogMessage(string message, Color color)
+    {
+        string formattedMessage = $"<color=\"{ColorUtility.ToHtmlStringRGB(color)}\">{DateTime.Now.ToString("HH:mm:ss.fff")} {message}</color>\n";
+        debugAreaText.text += formattedMessage;
+        ClearLines();
+    }
+
+    public void LogInfo(string message)
+    {
+        LogMessage(message, Color.green);
+    }
+
+    public void LogError(string message)
+    {
+        LogMessage(message, Color.red);
+    }
+
+    public void LogWarning(string message)
+    {
+        LogMessage(message, Color.yellow);
+    }
+
+    private void ClearLines()
+    {
+        string[] lines = debugAreaText.text.Split('\n');
+        if (lines.Length >= maxLines)
+        {
+            debugAreaText.text = string.Join("\n", lines.Skip(lines.Length - maxLines));
+        }
+    }
+}
