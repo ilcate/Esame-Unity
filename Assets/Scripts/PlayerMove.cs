6451a327279de1111db499b7da987a5cb9c1b0c9
ilcate <124363106+ilcate@users.noreply.github.com>
2024-05-09 09:03:31 +0200

    some changes

--- Assets/Scripts/PlayerMove.cs
+++ Assets/Scripts/PlayerMove.cs
@@ -6,22 +6,34 @@ using Unity.VisualScripting;
 
 public class PlayerMove : NetworkBehaviour
 {
-    public float speed = 5f;
- 
+    public float speed = 10f;
+    Rigidbody rb;
+
 
     public override void OnNetworkSpawn()
     {
+        //conto quanti ce ne sono prima e così capisco il numero del mio player
+        //e da li do spawnpoint custom
+
         if (!IsOwner) Destroy(this);
+
+        transform.position = new Vector3(-10, 0f, -5);
     }
-    
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
 
     void Update()
     {
+
         float moveHorizontal = Input.GetAxis("Horizontal");
         float moveVertical = Input.GetAxis("Vertical");
 
-        Vector3 movement = new Vector3(moveHorizontal, 0f, moveVertical);
 
-        transform.Translate(movement * speed/ 10);
+        rb.velocity = new Vector3(moveHorizontal, 0, moveVertical) * speed;
+
     }
 }
