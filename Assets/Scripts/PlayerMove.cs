0a34d7976a2838505553e7ffaf0074cec26ee168
ilcate <124363106+ilcate@users.noreply.github.com>
2024-05-15 18:13:15 +0200

    cazzo

--- Assets/Scripts/PlayerMove.cs
+++ Assets/Scripts/PlayerMove.cs
@@ -7,20 +7,62 @@ using TMPro;
 
 
 
+  
+
+
 public class PlayerMove : NetworkBehaviour
 {
     public float speed = 10f;
     public float rotationSpeed = 360f;
     private static string passed;
-
+    
 
     Animator animator;
     public static PlayerMove Instance { get; private set; }
 
     Rigidbody rb;
 
+    [SerializeField]
+    Texture2D rougeTexture;
 
-   
+    //rouge verde
+    //default viola
+    //
+
+    //[SerializeField]
+    //Texture2D rougeTexture;
+
+    //[SerializeField]
+    //Texture2D rougeTexture;
+
+    //[SerializeField]
+    //Texture2D rougeTexture;
+
+    [SerializeField]
+    GameObject armLeft;
+
+    [SerializeField]
+    GameObject armRight;
+
+    [SerializeField]
+    GameObject body;
+
+    [SerializeField]
+    GameObject head;
+
+    [SerializeField]
+    GameObject legLeft;
+
+    [SerializeField]
+    GameObject legRight;
+
+    [SerializeField]
+    GameObject hat;
+
+
+
+
+
     public static void PassName(string inputUsername)
     {
         passed = inputUsername;
@@ -35,11 +77,11 @@ public class PlayerMove : NetworkBehaviour
 
         if (!IsOwner) Destroy(this);
 
-       
+        //Texture2D textureRouge = (Texture2D)Resources.Load("Assets/PlayerModels/fbx/rogue_texture.png");
 
-        //PlayerInfo.AssignName(passed);
-
-
+        //Debug.Log(textureRouge);
+        hat.GetComponent<Renderer>().material.mainTexture = rougeTexture;
+        //ChangeSkin();
         transform.position = new Vector3(Random.Range(-20, 20), 0f, Random.Range(-20, 20));
     }
 
