faabca05cd61067d6c2df8152c1d6ba795e30c85
Alessandro Mita <123252161+Unpollooooooo@users.noreply.github.com>
2024-06-25 15:02:08 +0200

    boh

--- Assets/Scripts/projectileMove.cs
+++ Assets/Scripts/projectileMove.cs
@@ -88,6 +88,7 @@ public class ProjectileMove : NetworkBehaviour
             if (playerShooting != null)
             {
                 playerShooting.DisableShooting();
+
             }
         }
 
