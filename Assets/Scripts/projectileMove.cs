85c954422065ac69e5c46f4b3fd9da3df74ef6f0
ilcate <124363106+ilcate@users.noreply.github.com>
2024-06-26 16:05:12 +0200

    test

--- Assets/Scripts/projectileMove.cs
+++ Assets/Scripts/projectileMove.cs
@@ -16,10 +16,7 @@ public class ProjectileMove : NetworkBehaviour
 
     public float lastSyncTime = 0f;
     private float syncInterval = 0.1f;
-    private float curSpeed;
-    private int curBounces = 0;
-    private Vector3 lastVelocity;
-    private Vector3 direction;
+
 
     void Start()
     {
@@ -101,12 +98,9 @@ public class ProjectileMove : NetworkBehaviour
             }
         }
 
-        if (isBounceShot && curBounces >= remainingBounces)
+        if (isBounceShot && remainingBounces > 0)
         {
-            curSpeed = lastVelocity.magnitude;
-            direction = Vector3.Reflect(lastVelocity.normalized, collision.contacts[0].normal);
-            rb.velocity = direction * Mathf.Max(curSpeed, 0);
-            curBounces++;
+            remainingBounces--;
         }
 
         else
